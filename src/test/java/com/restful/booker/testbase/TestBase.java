834a565ef389ab016d1476a12bc4dffedc5d60ff
nimishapatel1987 <nimisha.patel1987@gmail.com>
2023-01-03 22:24:22 +0000

    Week18-Project2-restful-booker-serenity_NimishaPatel

---
+++ src/test/java/com/restful/booker/testbase/TestBase.java
@@ -0,0 +1,23 @@
+package com.restful.booker.testbase;
+
+import com.restful.booker.utils.PropertyReader;
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+import java.nio.file.Path;
+
+/**
+ * Created by Jay
+ */
+public class TestBase {
+    public static PropertyReader propertyReader;
+
+    @BeforeClass
+    public static void init() {
+        propertyReader = PropertyReader.getInstance();
+        RestAssured.baseURI = propertyReader.getProperty("baseUrl");
+     //   RestAssured.port = Integer.parseInt(propertyReader.getProperty("port"));
+    //    RestAssured.basePath = Path.BOOKING;
+    }
+
+}
